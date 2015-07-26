7c2414a8cb01c239ed53795ce8ad9d196c91822e
Stefan Schlott <stefan@ploing.de>
2015-07-26 22:30:01 +0200

    Stub for meetup silhouette provider

---
+++ app/lib/silhouette/MeetupProvider.scala
@@ -0,0 +1,39 @@
+package lib.silhouette
+
+import com.mohiva.play.silhouette.api.util.HTTPLayer
+import com.mohiva.play.silhouette.impl.providers._
+import play.api.libs.json.JsValue
+
+
+object MeetupProvider {
+  val ID = "meetup"
+  val API = "http://whats.this.for?"
+}
+
+
+trait BaseMeetupProvider extends OAuth2Provider {
+  override type Content = JsValue
+
+  override val id = MeetupProvider.ID
+
+  override protected val urls = Map("api" -> MeetupProvider.API)
+
+  override protected def buildProfile(authInfo: OAuth2Info) = ???
+}
+
+
+class MeetupProfileParser extends SocialProfileParser[JsValue, CommonSocialProfile] {
+  override def parse(content: JsValue) = ???
+}
+
+
+class MeetupProvider(protected val httpLayer: HTTPLayer,
+                     protected val stateProvider: OAuth2StateProvider,
+                     val settings: OAuth2Settings)
+  extends BaseMeetupProvider with CommonSocialProfileBuilder {
+
+  type Self = MeetupProvider
+  val profileParser = new MeetupProfileParser
+
+  override def withSettings(f: (OAuth2Settings) => OAuth2Settings) = ???
+}
--- app/modules/SilhouetteModule.scala
+++ app/modules/SilhouetteModule.scala
@@ -2,15 +2,19 @@ package modules
 
 import com.google.inject.{AbstractModule, Provides}
 import com.mohiva.play.silhouette.api.services.AuthenticatorService
-import com.mohiva.play.silhouette.api.util.{Clock, FingerprintGenerator}
+import com.mohiva.play.silhouette.api.util._
 import com.mohiva.play.silhouette.api.{Environment, EventBus}
 import com.mohiva.play.silhouette.impl.authenticators.{SessionAuthenticator, SessionAuthenticatorService, SessionAuthenticatorSettings}
-import com.mohiva.play.silhouette.impl.providers.SocialProviderRegistry
-import com.mohiva.play.silhouette.impl.util.DefaultFingerprintGenerator
+import com.mohiva.play.silhouette.impl.providers.oauth2.GoogleProvider
+import com.mohiva.play.silhouette.impl.providers.oauth2.state.{CookieStateProvider, CookieStateSettings}
+import com.mohiva.play.silhouette.impl.providers.{OAuth2Settings, OAuth2StateProvider, SocialProviderRegistry}
+import com.mohiva.play.silhouette.impl.util.{SecureRandomIDGenerator, DefaultFingerprintGenerator}
+import lib.silhouette.MeetupProvider
 import models.{User, UserService}
 import models.impl.UserServiceImpl
 import net.codingwell.scalaguice.ScalaModule
 import play.api.Configuration
+import play.api.libs.ws.WSClient
 
 
 class SilhouetteModule extends AbstractModule with ScalaModule {
@@ -18,6 +22,7 @@ class SilhouetteModule extends AbstractModule with ScalaModule {
 
   override def configure() = {
     bind[UserService].to[UserServiceImpl]
+    bind[IDGenerator].toInstance(new SecureRandomIDGenerator())
     bind[FingerprintGenerator].toInstance(new DefaultFingerprintGenerator(false))
     bind[EventBus].toInstance(EventBus())
     bind[Clock].toInstance(Clock())
@@ -48,7 +53,29 @@ class SilhouetteModule extends AbstractModule with ScalaModule {
   }
 
   @Provides
-  def provideSocialProviderRegistry(): SocialProviderRegistry = {
-    SocialProviderRegistry(Seq())
+  def provideSocialProviderRegistry(meetupProvider: MeetupProvider): SocialProviderRegistry = {
+    SocialProviderRegistry(Seq(meetupProvider))
+  }
+
+  @Provides
+  def provideHTTPLayer(client: WSClient): HTTPLayer = new PlayHTTPLayer(client)
+
+  @Provides
+  def provideOAuth2StateProvider(idGenerator: IDGenerator, configuration: Configuration, clock: Clock): OAuth2StateProvider = {
+    import net.ceedubs.ficus.Ficus._
+    import net.ceedubs.ficus.readers.ArbitraryTypeReader._
+
+    val settings = configuration.underlying.as[CookieStateSettings]("silhouette.oauth2StateProvider")
+    new CookieStateProvider(settings, idGenerator, clock)
+  }
+
+  @Provides
+  def provideMeetupProvider(httpLayer: HTTPLayer,
+                            stateProvider: OAuth2StateProvider,
+                            configuration: Configuration): MeetupProvider = {
+    import net.ceedubs.ficus.Ficus._
+    import net.ceedubs.ficus.readers.ArbitraryTypeReader._
+
+    new MeetupProvider(httpLayer, stateProvider, configuration.underlying.as[OAuth2Settings]("silhouette.meetup"))
   }
 }
