34a4e22751d4934e08336c09dd8ffcfbe0ad7466
Stefan Schlott <stefan@ploing.de>
2015-07-27 19:04:19 +0200

    Implementation of MeetupProvider completed

--- app/lib/silhouette/MeetupProvider.scala
+++ app/lib/silhouette/MeetupProvider.scala
@@ -1,13 +1,19 @@
 package lib.silhouette
 
+import com.mohiva.play.silhouette.api.LoginInfo
 import com.mohiva.play.silhouette.api.util.HTTPLayer
+import com.mohiva.play.silhouette.impl.exceptions.ProfileRetrievalException
 import com.mohiva.play.silhouette.impl.providers._
+import play.api.Logger
 import play.api.libs.json.JsValue
+import play.api.http.HeaderNames._
+
+import scala.concurrent.Future
 
 
 object MeetupProvider {
   val ID = "meetup"
-  val API = "http://whats.this.for/?"
+  val API = "https://api.meetup.com/2/member/self?&sign=true"
 }
 
 
@@ -18,12 +24,25 @@ trait BaseMeetupProvider extends OAuth2Provider {
 
   override protected val urls = Map("api" -> MeetupProvider.API)
 
-  override protected def buildProfile(authInfo: OAuth2Info) = ???
+  override protected def buildProfile(authInfo: OAuth2Info): Future[Profile] = {
+    httpLayer.url(urls("api")).withHeaders(AUTHORIZATION -> s"Bearer ${authInfo.accessToken}").get().flatMap { response =>
+      response.status match {
+        case 200 => profileParser.parse(response.json)
+        case _ => throw new ProfileRetrievalException(response.statusText)
+      }
+    }
+  }
 }
 
 
 class MeetupProfileParser extends SocialProfileParser[JsValue, CommonSocialProfile] {
-  override def parse(content: JsValue) = ???
+  override def parse(content: JsValue) = Future.successful {
+    CommonSocialProfile(
+      loginInfo = LoginInfo(MeetupProvider.ID, (content \ "id").as[Int].toString),
+      fullName = (content \ "name").asOpt[String],
+      avatarURL = (content \ "photo" \ "thumb_link").asOpt[String]
+    )
+  }
 }
 
 
@@ -35,5 +54,5 @@ class MeetupProvider(protected val httpLayer: HTTPLayer,
   type Self = MeetupProvider
   val profileParser = new MeetupProfileParser
 
-  override def withSettings(f: (OAuth2Settings) => OAuth2Settings) = ???
+  override def withSettings(f: (Settings) => Settings) = new MeetupProvider(httpLayer, stateProvider, f(settings))
 }
