755a8a8e1c17e7d831d750e047814b513867779f
Stefan Schlott <stefan@ploing.de>
2015-09-17 21:56:32 +0200

    Non-persistent data storage

--- app/models/pizza/Pizza.scala
+++ app/models/pizza/Pizza.scala
@@ -1,3 +1,10 @@
 package models.pizza
 
+import scala.collection.mutable
+
+
 case class Pizza(id: Int, name: String)
+
+object PizzaRepo {
+  val pizzas = new mutable.HashMap[Int, Pizza]
+}
--- app/models/pizza/Vote.scala
+++ app/models/pizza/Vote.scala
@@ -1,4 +1,10 @@
 package models.pizza
 
+import scala.collection.mutable
+
 
 case class Vote(pizzaId: Int, userProviderKey: String, vote: Int)
+
+object VoteRepo {
+  val votes = new mutable.MutableList[Vote]()
+}
